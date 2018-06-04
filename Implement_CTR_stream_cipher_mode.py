2cc6a8194a3a0c6ff36a1f8086e87cf8967a3416
Neelam S G <39006317+nsg1999@users.noreply.github.com>
2018-06-04 16:37:14 +0530

    Update and rename CTR_mode.py to Implement_CTR_stream_cipher_mode.py

--- Implement_CTR_stream_cipher_mode.py
+++ Implement_CTR_stream_cipher_mode.py
@@ -1,25 +1,27 @@
 from Crypto.Cipher import AES
+import struct
+
 
 key = "YELLOW SUBMARINE"
 
 _input = "L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ==".decode("base64") 
 
-
 nonce = 0
 c = 0
 
-def count_func(nonce, c):
-	inc_c = nonce + c
+def count_func():
+	global c
+	n = struct.pack("<Q", nonce)
+	c1 = struct.pack("<Q", c)
+	inc_c = n + c1
 	c = c + 1
 	return inc_c
 	
 	
 
-
-counter = str(count_func(nonce,c))
-
-cipher = AES.new(key,AES.MODE_CTR,counter)
-
+cipher = AES.new(key,AES.MODE_CTR,counter=count_func)
 answer = cipher.decrypt(_input)
 
+print(answer)
+print("--------")
     
